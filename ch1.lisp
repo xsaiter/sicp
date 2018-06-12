a55b2924956540d7b317b0356775047919bc5ff5
xsaiter <xsaiter@mail.ru>
2018-06-12 17:05:35 +0300

    improve

--- ch1.lisp
+++ ch1.lisp
@@ -1,10 +1,32 @@
-(defun len (lst)
+(defun our-len (lst)
   (if (null lst)
       0
-      (+ (len (cdr lst)) 1)))
+      (+ (our-len (cdr lst)) 1)))
 
-(defun len-i (lst)
+(defun our-len-i (lst)
   (let ((res 0))
     (dolist (x lst)
       (setf res (+ res 1)))
     res))
+
+(defun our-cube (x) (* x x x))
+
+(defun our-square (x) (* x x))
+
+
+;;; 1.3
+(defun our-sum (a b term next)
+  (if (> a b)
+      0
+      (+ (funcall term a)
+	 (our-sum (funcall next a) b term next))))
+
+(defun our-self (x) x)
+
+(defun our-inc (x) (+ x 1))
+
+(defun our-sum-integers (a b)
+  (our-sum a b #'our-self #'our-inc))
+
+(defun our-sum-cubes ( a b)
+  (our-sum a b #'our-cube #'our-inc))
