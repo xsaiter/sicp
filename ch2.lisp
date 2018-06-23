9c5516ac3c26e76d54811dd5c5164fb6fe676e0f
xsaiter <xsaiter@mail.ru>
2018-06-23 19:22:44 +0300

    improve

--- ch2.lisp
+++ ch2.lisp
@@ -1,7 +1,11 @@
-(defun var-p (x)
+;;;
+;;; derivation
+;;;
+
+(defun var? (x)
   (symbolp x))
 
-(defun same-var-p (x1 x2)
+(defun same-vars? (x1 x2)
   (and (var-p x1)
        (var-p x2)
        (equal x1 x2)))
@@ -11,7 +15,7 @@
 (defun make-add (x1 x2)
   (list '+ x1 x2))
 
-(defun add-p (x)
+(defun add? (x)
   (and (consp x)
        (equal (car x) '+)))
 
@@ -26,7 +30,7 @@
 (defun make-mul (x1 x2)
   (list '* x1 x2))
 
-(defun mul-p (x)
+(defun mul? (x)
   (and (consp x)
        (equal (car x) '*)))
 
@@ -36,15 +40,37 @@
 (defun mul-elt-2 (x)
   (third x))
 
-;; derivation
-
 (defun deriv (exp x)
   (cond ((numberp exp) 0)
-	((var-p exp)
-	 (if (same-var-p exp x) 1 0))
-	((add-p exp)
+	((var? exp)
+	 (if (same-vars? exp x) 1 0))
+	((add? exp)
 	 (make-add (deriv (add-elt-1 exp) x)
 		   (deriv (add-elt-2 exp) x)))
-	((mul-p exp)
+	((mul? exp)
 	 (make-mul (deriv (mul-elt-1 exp) x)
 		   (deriv (mul-elt-2 exp) x)))))	
+
+
+;;;
+;;; set
+;;;
+
+(defun contains-set? (x set)
+  (cond
+    ((null set) nil)
+    ((equal x (car set)) x)
+    (t (contains-set? x (cdr x)))))
+    
+
+(defun add-to-set (x set)
+  (if (contains-set? x set)
+      set
+      (cons x set)))
+
+(defun intersection-set (set1 set2)
+  (cond (or (null set1) (null set2) '())
+	(contains-set? (car set1) set2)
+	(cons (car set1)
+	      (intersection-set (cdr set1) set2))
+	(t (intersection-set (cdr set1) set2))))
